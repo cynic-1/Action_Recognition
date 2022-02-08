133e51440f7b754280f6d8f64066f94dadb92312
song <songyx2001@126.com>
2022-02-08 22:54:32 +0800

    视频切片

--- dev/back_end/app/controller/video.controller.js
+++ dev/back_end/app/controller/video.controller.js
@@ -59,6 +59,53 @@ export const getVideo = (req, res) => {
 
                 readStream.pipe(res);
             })
+            .catch(err => {
+                res.status(500).send({
+                    message:
+                        err.message || "Some error occurred while storing the video."
+                });
+            })
 
-    }
+}
+
+export const getVideoChunk = function(req, res) {
+    const vid = req.params.id;
+    Video.findById(vid)
+        .then(data => {
+            const videoPath = path.resolve(__dirname, '../../' + data.raw)
+            const stat = fs.statSync(videoPath)
+            const fileSize = stat.size
+            const range = req.headers.range
+            if (range) {
+                const parts = range.replace(/bytes=/, "").split("-")
+                const start = parseInt(parts[0], 10)
+                const end = parts[1]
+                    ? parseInt(parts[1], 10)
+                    : fileSize-1
+                const chunksize = (end-start)+1
+                const file = fs.createReadStream(videoPath, {start, end})
+                const head = {
+                    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                    'Accept-Ranges': 'bytes',
+                    'Content-Length': chunksize,
+                    'Content-Type': 'video/mp4',
+                }
+                res.writeHead(206, head);
+                file.pipe(res);
+            } else {
+                const head = {
+                    'Content-Length': fileSize,
+                    'Content-Type': 'video/mp4',
+                }
+                res.writeHead(200, head)
+                fs.createReadStream(videoPath).pipe(res)
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while storing the video."
+            });
+        })
+}
 
