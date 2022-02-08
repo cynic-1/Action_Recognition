040e01a203534d93359f49e1c774f497445d4a72
Shihesy <54487697+Zera-Algorithm@users.noreply.github.com>
2022-02-08 17:51:55 +0800

    添加关键点标记和排球标记

--- python/main.py
+++ python/main.py
@@ -11,8 +11,12 @@ import numpy
 import numpy as np
 
 import angle
+import keypoints
 
-is_low_resolution = True
+# 引入排球识别
+from detectron2_package import VolleyballDetect as Detect
+
+is_low_resolution = False
 
 
 # 用来将参数字典转化为命令行参数字符串
@@ -35,10 +39,10 @@ def produce_json():
 
     params = dict()
     # 这些路径都是相对于根路径而言的路径
-    params["image_dir"] = os.path.join(dir_path, "images")
+    params["image_dir"] = "\"" + os.path.join(dir_path, "images") + "\""
     # params["write_images"] = os.path.join(dir_path, "output_images")
     params["model_pose"] = "BODY_25"
-    params["write_json"] = os.path.join(dir_path, "output_json")
+    params["write_json"] = "\"" + os.path.join(dir_path, "output_json") + "\""
 
     if is_low_resolution:
         params["net_resolution"] = "320x176"  # 调低分辨率，以让低显存电脑也能顺利运行
@@ -51,14 +55,21 @@ def produce_json():
     information.close()
 
 
+def hcolor_to_bgr(hcolor):
+    r = int(hcolor[1:3], 16)
+    g = int(hcolor[3:5], 16)
+    b = int(hcolor[5:7], 16)
+    return (b, g, r)
+
 
 if __name__ == "__main__":
-    produce_json()  # 先在output_json目录下输出json文件
+    # produce_json()  # 先在output_json目录下输出json文件
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     os.chdir(dir_path) # 切换回原来的路径
 
     img = cv2.imread("./images/1.jpg")  # numpy.ndarray类型
+    img_origin = img.copy()
     print(img.shape)  # 输出 几行几列几维颜色
 
     # 读取和转化JSON文件
@@ -72,11 +83,37 @@ if __name__ == "__main__":
         pose_keypoints_2d = json_dict["people"][0]["pose_keypoints_2d"]
         for i in range(25):
             humanpoints[0][i] = [pose_keypoints_2d[i*3], pose_keypoints_2d[i*3+1], pose_keypoints_2d[i*3+2]]
+
+        for i in range(25):
             x, y, confidence = pose_keypoints_2d[i*3], pose_keypoints_2d[i*3+1], pose_keypoints_2d[i*3+2]
             
             # 确保confidence参数不为0
-            if abs(confidence) > 0.00001:
-                cv2.circle(img, (int(x), int(y)), 10, (0, 0, int(255*confidence)), -1)  #在图中画出关键点
+            if abs(confidence) > 0.0001:
+                if(i < len(keypoints.color_table)):
+                    color = hcolor_to_bgr(keypoints.color_table[i])
+                    cv2.circle(img, (int(x), int(y)), 8, color, -1)  #在图中画出关键点
+            else:
+                print(f"keypoint {i} not exists.")
+
+        for i in range(25):
+            x, y, confidence = pose_keypoints_2d[i*3], pose_keypoints_2d[i*3+1], pose_keypoints_2d[i*3+2]
+            
+            # 确保confidence参数不为0
+            if abs(confidence) > 0.0001:
+                if(i < len(keypoints.color_table)):
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    cv2.putText(img,str(i),(int(x)-2,int(y)+4),font,0.3,(255,255,255),1,cv2.LINE_AA)
+
+    # 获取球的信息
+    boxes = Detect.detect_ball(img_origin)
+    print(f"检测到{len(boxes)}个球。")
+    for box in boxes:
+        x1, y1, x2, y2 = box
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        # 以绿色边框描出球的位置
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        print(f"球的长度是{x2-x1}")
+
 
     #打印角度 -1 代表不构成三角形
     for i in range(people_cnt):
@@ -99,8 +136,9 @@ if __name__ == "__main__":
         newH = 700
         newW = int(newH * (W / H))
 
-    cv2.namedWindow("image", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("image", newW, newH) # 重设窗体宽高
-    cv2.imshow("image", img)
-    cv2.waitKey(0)
+    # cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+    # cv2.resizeWindow("image", newW, newH) # 重设窗体宽高
+    # cv2.imshow("image", img)
+    cv2.imwrite("img.jpg", img)
+    # cv2.waitKey(0)
     pass
